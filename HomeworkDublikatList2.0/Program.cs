d2941d154fd26458096b3e71a4cf541ef0939000
mike19952006 <mike19952006@gmail.com>
2018-03-09 22:02:57 +0300

    Без Remove2.0

--- HomeworkDublikatList2.0/Program.cs
+++ HomeworkDublikatList2.0/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeworkDublikatList2._0
+namespace HomeworkDublikat
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<int> listarray = new List<int>()
+            {
+                1,2,3,3,7,12,3,3,7,12,3,3,7,12,3,3,7,12,3,3,7,12,3,3,7,1
+            };
+            List<int> listarrayNew = new List<int>();
+
+
+            for (int i = 0; i < listarray.Count; i++)
+            {
+                bool b = false;
+                for (int j = 0; j < listarrayNew.Count; j++)
+                {
+                    if (listarray[i] == listarrayNew[j])
+                    {
+                        b = true;
+                        break;
+                    }
+                }
+                if (b == false)
+                    listarrayNew.Add(listarray[i]);
+            }
+
+
+            for (int y = 0; y < listarrayNew.Count; y++)
+            {
+                Console.WriteLine(listarrayNew[y]);
+            }
+
         }
     }
 }
